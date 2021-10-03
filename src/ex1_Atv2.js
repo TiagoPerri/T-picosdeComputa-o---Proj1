d843d2eec030c1d3a4d5287b5fbf1c6e63e58eb4
TiagoPerri <tiagocnsr@gmail.com>
2021-10-03 13:54:41 -0300

    Traduzindo GerarNumeroAleatorio() para Javascript

--- src/ex1_Atv2.js
+++ src/ex1_Atv2.js
@@ -1,1 +1,14 @@
-module.exports = ;
+function GerarNumeroAleatorio(inicio, fim) {
+    if (inicio < 0 || fim < 0) {
+        return -1;
+    }
+
+    if (inicio >= fim) {
+        return -1;
+    }
+
+    const aux = Math.random();
+    return aux * (fim - inicio) + inicio;
+}
+
+module.exports = GerarNumeroAleatorio;
